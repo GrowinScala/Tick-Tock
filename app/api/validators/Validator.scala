9245e6069d9df169d40f09b44d8b7d207603ea68
megidio <37144190+megidio@users.noreply.github.com>
2018-12-14 15:39:48 +0000

    Update Validator.scala

--- app/api/validators/Validator.scala
+++ app/api/validators/Validator.scala
@@ -56,44 +56,4 @@ object Validator {
     }
 
   }
-
-
-
-
-
-
-
-
-  /*
-  def isValidLength(string: String, maxLength: Int): Boolean = {
-    string.length <= maxLength
-  }
-
-  def isValidDate(date: String): Int = {
-
-    def iter(array: Array[SimpleDateFormat], index: Int): Int = {
-      try{
-        array.head.setLenient(false)
-        val d = array.head.parse(date)
-        val s = array.head.format(d)
-        index
-      }
-      catch{
-        case _: Throwable =>
-          if(array.tail.isEmpty) -1
-          else iter(array.tail, index + 1)
-      }
-
-    }
-    iter(dateFormatsList, 0)
-  }
-
-  def isValidFilePath(filepath: String): Boolean = {
-    val defaultFS = FileSystems.getDefault()
-    val separator = defaultFS.getSeparator()
-    val path = defaultFS.getPath(filepath)
-    Files.exists(path)
-  }
-
-  def isValidFileName(name: String): Boolean = ???*/
-}
+ }
